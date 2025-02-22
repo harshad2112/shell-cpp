eab748f540609dc2a1eb989c0efb50bb64e44583
harshad2112 <harshadaggarwal1999@gmail.com>
2025-02-23 00:37:55 +0530

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    while(true)
+    {
+        std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout<<input<<": command not found"<<'\n';
+        std::string input;
+        std::getline(std::cin, input);
+        std::cout<<input<<": command not found"<<'\n';
+    }
 }
